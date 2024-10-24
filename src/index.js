a886bf3fe6d62b0fb750dc9ce4990fce47a0302e
DieSILV <59703636+DieSILV@users.noreply.github.com>
2024-10-24 14:52:38 +0200

    fix: Solutions js countdown

--- src/index.js
+++ src/index.js
@@ -1,38 +1,61 @@
-const DURATION = 10; // 10 seconds
-let remainingTime = DURATION; // Countdown starting from 10
-let timer = null; // Variable to store the interval
+document.addEventListener("DOMContentLoaded", () => {
 
+  const DURATION = 10; // 10 seconds
+  let timer = null; // Variable to store the interval
+  const startBtn = document.querySelector("#start-btn");
+  startBtn.addEventListener("click", startCountdown);
+  const time = document.querySelector("#time");
 
+  // ITERATION 1: Add event listener to the start button
+  function startCountdown() {
+    timer = DURATION;
+    startBtn.disabled = true
+    const intervalTimer = setInterval(() => {
+      if (timer === 10) {
+        let message = '⏰ Final countdown! ⏰'
+        showToast(message)
+      }
+      if (timer === 5) {
+        let message = 'Start the engines! 💥'
+        showToast(message)
+      }
+      if (timer > 0) {
+        timer--
+        time.innerHTML = `${timer}`
+      }
 
-// ITERATION 1: Add event listener to the start button
+      if (timer <= 0) {
+        let message = 'Lift off! 🚀'
+        showToast(message)
+        reset()
+      }
+    }, 1000)
+  }
 
-// Your code goes here ...
+  function reset() {
+    startBtn.disabled = false
+    time.innerHTML = `${DURATION}`
+  }
 
+  //Toast visibilidad y texto
+  const toast_message = document.querySelector("#toast-message");
+  const show = document.getElementById("toast");
 
+  // ITERATION 3: Show Toast
+  function showToast(message) {
+    show.classList.add("show");
+    toast_message.innerHTML = `${message}`
+    setTimeout(() => {
+      show.classList.remove("show");
+    }, 2000);
+  }
 
+  //Closed toast manual
+  const close = document.getElementById("close-toast");
+  const closeButton = document.querySelector("#close-toast");
+  closeButton.addEventListener("click", () => {
+    close.classList.remove("show");
+    clearTimeout(show.classList.remove("show"), 3000);
+  });
 
-// ITERATION 2: Start Countdown
-function startCountdown() {
-  console.log("startCountdown called!");
-
-
-  // Your code goes here ...
-}
-
-
-
-
-// ITERATION 3: Show Toast
-function showToast(message) {
-  console.log("showToast called!");
-
-  // Your code goes here ...
-
-
-
-
-  // BONUS: ITERATION 4: TOAST CLOSE BUTTON
-
-  // Your code goes here ...
-
-}
+});
